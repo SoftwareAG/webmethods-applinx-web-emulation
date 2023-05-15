646b93480de903a5d04fd6c9fd8bbca847637946
Jaya Kusireddi <jkus@softwareag.com>
2023-05-15 16:30:04 +0530

    Code changes for APX-14075

---
+++ src/utils/GXColorTheme.ts
@@ -0,0 +1,221 @@
+export declare class ColorThemeProperties {
+  'bg-color': string;
+  'input-text-bg-color': string;
+  'input-text-border-bottom': string;
+  'text-color': string;
+  'input-text-pw': string;
+  'input-text': string;
+  'screen-btn-color': string;
+  'screen-btn-text-color': string;
+  'btn-regular-color': string;
+  'btn-outline-color': string
+  'btn-hover-color-screen': string;
+  'btn-hover-color': string;
+  'enter-btn-hover-color': string;
+  'enter-btn-text-hover-color': string;
+  'login-screen-color': string;
+  'menu-content-color': string;
+  'table-header-background': string;
+  'table-header-border-color-left': string;
+  'table-header-border-color-right': string;
+  'table-header-border-color-top': string;
+  'table-header-border-color-bottom': string;
+  'table-header-text-color': string;
+  'table-body-border-color': string;
+  'table-body-alternating1': string;
+  'table-body-alternating2': string
+  'table-body-text-color': string
+  'table-body-select-border-color': string
+  'table-body-select-background-color': string;
+  'table-body-select-color': string;
+  'table-text-shadow': string;
+  'input-bottom-border-gx-lgrn': string;
+  'cross-text-color': string;
+  'gx-line': string;    
+  'gx-blwt-text-color': string;
+  'gx-lbl': string;
+  'gx-lgrn': string;
+  'gx-lrd': string;
+  'gx-blbl': string;
+  'gx-blbl-text-color': string;
+  'gx-blrd': string;
+  'gx-lppl': string;
+  'gx-laq': string;
+  'gx-ylw': string;
+  'gx-blgrn': string;
+  'gx-blgrn-text-color': string;
+  'gx-blaq': string;
+  'gx-blaq-text-color': string;
+  'gx-bylw': string;
+  'gx-bylw-text-color': string;
+  'gx-intf': string;
+  'text-shadow-gx-lrd-intf': string
+  'text-shadow-gx-lwt-gx-intf': string;
+}
+
+export enum BlackTheme {
+  'bg-color' = 'black',
+  'input-text-bg-color' = 'black',
+  'input-text-border-bottom'= 'solid',
+  'text-color' = 'white',
+  'input-text-pw' = '#ff0000',
+  'input-text' = '#00ff00',
+  'screen-btn-color' = '#e3e3e3',
+  'screen-btn-text-color'= 'black',
+  'btn-regular-color' = '#1776bf',
+  'btn-outline-color' = '#E57200',
+  'btn-hover-color-screen' = '#E57200',
+  'btn-hover-color' = '#E57200',
+  'enter-btn-hover-color' = '#E57200',
+  'enter-btn-text-hover-color' = 'white',
+  'login-screen-color' = '#59b0ca',
+  'menu-content-color' = 'white',
+  'table-header-background' = '#006992',
+  'table-header-border-color-left' = '#042237',
+  'table-header-border-color-right' = 'none',
+  'table-header-border-color-top' = 'none',
+  'table-header-border-color-botton' = 'none',
+  'table-header-text-color' = 'white',
+  'table-body-border-color' = '#042237',
+  'table-body-alternating1' = '#042237',
+  'table-body-alternating2' = 'black',
+  'table-body-text-color' = '#59b0ca',
+  'table-body-select-border-color' = 'black',
+  'table-body-select-background-color' = '#042237',
+  'table-body-select-color' = '#59b0ca',
+  'table-text-shadow' = 'none',
+  'input-bottom-border-gx-lgrn' = '#9DBD72',
+  'cross-text-color' = 'white',
+  'gx-line' = 'white',
+  'gx-blwt-text-color' = 'black',
+  'gx-lbl' = '#59B0CA',
+  'gx-lgrn' = '#9DBD72',
+  'gx-lrd' = '#CE5F68',
+  'gx-blbl' = '#006992',
+  'gx-blbl-text-color' = 'black',
+  'gx-blrd' = '#CE5F68',
+  'gx-lppl' = '#9D87B5',
+  'gx-laq' = '#99CEDE',
+  'gx-ylw' = '#ffd700',
+  'gx-blgrn' = '#9DBD72',
+  'gx-blgrn-text-color' = 'black',
+  'gx-blaq' = '#99CEDE',
+  'gx-blaq-text-color' = 'black',
+  'gx-bylw' = '#ffd700',
+  'gx-bylw-text-color' = 'black',
+  'gx-intf' = ' 0 0.025em 0 black, 0.025em 0 0 black',
+  'text-shadow-gx-lrd-intf' = 'rgba(255, 100, 100, 0.9) 0px 0px 15px',
+  'text-shadow-gx-lwt-gx-intf' = '0 0 15px rgba(255,255,255,0.9)'
+}
+
+export enum WhiteTheme {
+  'bg-color' = 'white',
+  'input-text-bg-color' = 'white',
+  'input-text-border-bottom'= 'solid',
+  'text-color' = 'black',
+  'input-text' = '#008000',
+  'input-text-pw' = '#AF272F',
+  'screen-btn-color' = '#e3e3e3',
+  'screen-btn-text-color'= 'black',
+  'btn-regular-color' = '#1776bf',
+  'btn-outline-color' = '#1776bf',
+  'btn-hover-color-screen' = '#068CB2',
+  'btn-hover-color' = '#068CB2',
+  'enter-btn-hover-color' = 'white',
+  'enter-btn-text-hover-color' = '#1776bf',
+  'login-screen-color' = '#006992',
+  'menu-content-color' = 'black',
+  'table-header-background' = '#093655',
+  'table-header-border-color-left' = '#d4f0f7',
+  'table-header-border-color-right' = 'none',
+  'table-header-border-color-top' = 'none',
+  'table-header-border-color-botton' = 'none',
+  'table-header-text-color' = 'white',
+  'table-body-border-color' = '#d4f0f7',
+  'table-body-alternating1' = 'white',
+  'table-body-alternating2' = '#d4f0f7',
+  'table-body-text-color' = '#006d93',
+  'table-body-select-border-color' = 'black',
+  'table-body-select-background-color' = 'white',
+  'table-body-select-color' = 'black',
+  'table-text-shadow' = 'none',
+  'input-bottom-border-gx-lgrn' = '#9DBD72',
+  'cross-text-color' = 'black',
+  'gx-line' = 'black',
+  'gx-blwt-text-color' = 'white',
+  'gx-lbl' = '#006D93',
+  'gx-lgrn' = '#9DBD72',
+  'gx-lrd' = '#CE5F68',
+  'gx-blbl' = '#006992',
+  'gx-blbl-text-color' = 'black',
+  'gx-blrd' = '#CE5F68',
+  'gx-lppl' = '#9D87B5',
+  'gx-laq' = '#99CEDE',
+  'gx-ylw' = '#ffd700',
+  'gx-blgrn' = '#9DBD72',
+  'gx-blgrn-text-color' = 'black',
+  'gx-blaq' = '#99CEDE',
+  'gx-blaq-text-color' = 'black',
+  'gx-bylw' = '#ffd700',
+  'gx-bylw-text-color' = 'black',
+  'gx-intf' = ' 0 0.025em 0 black, 0.025em 0 0 black',
+  'text-shadow-gx-lrd-intf' = '0 0.025em 0 #AF272F, 0.025em 0 0 #AF272F',
+  'text-shadow-gx-lwt-gx-intf' = '0 0.025em 0 black, 0.025em 0 0 black'
+
+}
+
+export enum GreenTheme {
+  'bg-color' = 'black',
+  'input-text-bg-color' = 'rgb(0, 64, 0)',
+  'input-text-border-bottom'= 'none',
+  'text-color' = 'lime',
+  'input-text' = 'lime',
+  'input-text-pw' = 'lime',
+  'screen-btn-color' = 'rgb(0, 64, 0)',
+  'screen-btn-text-color'= 'lime',
+  'btn-regular-color' = '#1776bf',
+  'btn-outline-color' = 'rgb(0, 102, 0)',
+  'btn-hover-color-screen' = 'rgb(0, 102, 0)',
+  'btn-hover-color' = 'rgb(0, 102, 0)',
+  'enter-btn-hover-color' = 'lime',
+  'enter-btn-text-hover-color' = 'black',
+  'login-screen-color' = 'lime',
+  'menu-content-color' = 'lime',
+  'table-header-background' = 'black',
+  'table-header-border-color-left' = 'lime',
+  'table-header-border-color-right' = 'lime',
+  'table-header-border-color-top' = 'lime',
+  'table-header-border-color-botton' = 'lime',
+  'table-header-text-color' = 'lime',
+  'table-body-border-color' = 'lime',
+  'table-body-alternating1' = 'rgb(0, 64, 0)',
+  'table-body-alternating2' = 'black',
+  'table-body-text-color' = 'lime',
+  'table-body-select-border-color' = 'black',
+  'table-body-select-background-color' = 'rgb(0, 64, 0)',
+  'table-body-select-color' = 'lime',
+  'table-text-shadow' = ' 0 0 0.6em lime',
+  'input-bottom-border-gx-lgrn' = 'lime',
+  'cross-text-color' = 'lime',
+  'gx-line' = 'lime',
+  'gx-blwt-text-color' = 'black',
+  'gx-lbl' = 'lime',
+  'gx-lgrn' = 'lime',
+  'gx-lrd' = 'lime',
+  'gx-blbl' = 'lime',
+  'gx-blbl-text-color' = 'black',
+  'gx-blrd' = 'lime',
+  'gx-lppl' = 'lime',
+  'gx-laq' = 'lime',
+  'gx-ylw' = 'lime',
+  'gx-blgrn' = 'lime',
+  'gx-blgrn-text-color' = 'black',
+  'gx-blaq' = 'lime',
+  'gx-blaq-text-color' = 'black',
+  'gx-bylw' = 'lime',
+  'gx-bylw-text-color' = 'black',
+  'gx-intf' = '0.01em 0.01em lime',
+  'text-shadow-gx-lrd-intf' = '0.01em 0.01em lime',
+  'text-shadow-gx-lwt-gx-intf' = '0.01em 0.01em lime'
+
+}
